678669d961c8028517627bb6ae44c6871638a9ee
Shad Malherbe <2130159@etu.cchic.ca>
2025-03-28 11:37:01 +0000

    Correction du fichier app.js pour preuve fonctionnement

--- web/app.js
+++ web/app.js
@@ -22,7 +22,7 @@ app.use(session({
 
 // Home route
 app.get('/', (req, res) => {
-    res.send('<h1>Welcome to the Authentication App</h1><form action="/login" method="POST"><input type="text" name="username" placeholder="Username" required/><input type="password" name="password" placeholder="Password" required/><button type="submit">Login</button></form>');
+    res.send('<h1>Welcome to the Authentication App - Shad</h1><form action="/login" method="POST"><input type="text" name="username" placeholder="Username" required/><input type="password" name="password" placeholder="Password" required/><button type="submit">Login</button></form>');
 });
 
 // Login route
